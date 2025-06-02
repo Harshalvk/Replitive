bf9f1e71bf7f0603de5f490c718279e142ebce89
Harshalvk <harshalvkhobragade@gmail.com>
2025-06-02 19:01:08 +0530

    feat: enhance CreateProjectDialog with project type selection and improved form layout

--- apps/web/app/(app)/dashboard/_components/CreateProjectDialog.tsx
+++ apps/web/app/(app)/dashboard/_components/CreateProjectDialog.tsx
@@ -28,6 +28,7 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
@@ -40,15 +41,41 @@ import {
 import { useMutation } from "@tanstack/react-query";
 import { CreateVirtualbox } from "@/actions/virtualbox/createVirtualbox";
 import { toast } from "sonner";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+type ProjectTypeOptionsT = "REACT" | "NODE";
+
+const projectType: {
+  id: ProjectTypeOptionsT;
+  name: string;
+  image: string;
+  description: string;
+}[] = [
+  {
+    id: "REACT",
+    name: "React",
+    image: "/project-icons/react.svg",
+    description: "A javascript library for building user interfaces",
+  },
+  {
+    id: "NODE",
+    name: "Node",
+    image: "/project-icons/node.svg",
+    description: "A javascript runtime built on V8 JavaScript Enginer",
+  },
+];
 
 const CreateProjectDialog = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedType, setSelectedType] =
+    useState<ProjectTypeOptionsT>("REACT");
 
   const form = useForm<z.infer<typeof createVirutalboxForm>>({
     resolver: zodResolver(createVirutalboxForm),
     defaultValues: {
       name: "",
-      type: "NODE",
+      type: "REACT",
       visibility: "PRIVATE",
     },
   });
@@ -86,11 +113,56 @@ const CreateProjectDialog = () => {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Create a new project</DialogTitle>
-          <DialogDescription>Start building your project</DialogDescription>
+          <DialogTitle>Create a VirutalBox</DialogTitle>
         </DialogHeader>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              disabled={isPending}
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Project Type</FormLabel>
+                  <FormControl>
+                    <div className="grid grid-cols-2 w-full gap-2">
+                      {projectType.map((type) => (
+                        <button
+                          type="button"
+                          key={type.id}
+                          className={cn(
+                            "w-full p-2 flex flex-col text-start relative border rounded-md overflow-hidden transition-all focus-visible:outline-none focus-visible:ring-offset-2 focus-visible:ring-offset-background focus-visible:right-2 focus-visible:ring-ring",
+                            selectedType && field.value === type.id
+                              ? "border-4"
+                              : "border-border"
+                          )}
+                          onClick={() => {
+                            setSelectedType(type.id);
+                            field.onChange(type.id);
+                          }}
+                        >
+                          <div className="w-full flex">
+                            <Image
+                              src={type.image}
+                              alt={type.name}
+                              width={20}
+                              height={20}
+                              className="absolute z-0 h-24 w-24 -top-6 -left-6 opacity-15"
+                            />
+                            <div className="z-10 font-semibold">
+                              {type.name}
+                            </div>
+                          </div>
+                          <div className="z-10 text-sm text-muted-foreground">
+                            {type.description}
+                          </div>
+                        </button>
+                      ))}
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
             <FormField
               disabled={isPending}
               control={form.control}
@@ -100,31 +172,6 @@ const CreateProjectDialog = () => {
                   <FormLabel>Project Name</FormLabel>
                   <FormControl>
                     <Input placeholder="Ex., Bolt clone" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your public display project name.
-                  </FormDescription>
-                </FormItem>
-              )}
-            />
-            <FormField
-              disabled={isPending}
-              control={form.control}
-              name="type"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Project Type</FormLabel>
-                  <FormControl>
-                    <Select value={field.value} onValueChange={field.onChange}>
-                      <SelectTrigger className="w-full">
-                        <SelectValue placeholder="Type" />
-                      </SelectTrigger>
-                      <SelectContent className="w-full">
-                        {VirtualBoxType.map((type) => (
-                          <SelectItem value={type}>{type}</SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
                   </FormControl>
                 </FormItem>
               )}
@@ -153,7 +200,12 @@ const CreateProjectDialog = () => {
                 </FormItem>
               )}
             />
-            <Button variant={"secondary"} type="submit" disabled={isPending}>
+            <Button
+              variant={"secondary"}
+              type="submit"
+              className="w-full"
+              disabled={isPending}
+            >
               Create{" "}
               {isPending && (
                 <Loader className="h-4 w-4 animate-spin transition-transform" />
@@ -161,13 +213,6 @@ const CreateProjectDialog = () => {
             </Button>
           </form>
         </Form>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant={"ghost"} disabled={isPending}>
-              Close
-            </Button>
-          </DialogClose>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
