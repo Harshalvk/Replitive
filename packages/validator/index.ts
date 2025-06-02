b0693daf96d55ada7f16a223602fdc46a6a69a80
Harshalvk <harshalvkhobragade@gmail.com>
2025-06-02 19:00:35 +0530

    feat: add validation for name field in createVirtualboxForm

--- packages/validator/index.ts
+++ packages/validator/index.ts
@@ -4,7 +4,9 @@ export const VirtualBoxType = ["REACT", "NODE"] as const;
 export const VirtualBoxVisibility = ["PUBLIC", "PRIVATE"] as const;
 
 export const createVirutalboxForm = z.object({
-  name: z.string(),
+  name: z
+    .string()
+    .min(5, { message: "Name should be atleast 5 characters long" }),
   type: z.enum(VirtualBoxType),
   visibility: z.enum(VirtualBoxVisibility),
 });
