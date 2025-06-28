e112324712b576a70bb9dcb910fc893f82334cbd
Harshalvk <harshalvkhobragade@gmail.com>
2025-06-28 08:40:54 +0530

    feat: add inngest to handle background jobs setup inngest client & functions configure inngest agent kit to get ai generatead content add /api/inngest route handle all inngest request

---
+++ src/inngest/functions.ts
@@ -0,0 +1,33 @@
+import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
+import { createAgent, gemini } from "@inngest/agent-kit";
+import { getSandbox } from "./utils";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("replitive-nextjs-test");
+      return sandbox.sandboxId;
+    });
+
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "you are an expert next.js developer. You write readable, maintaible code. You write simple Next.js & React snippets",
+      model: gemini({ model: "gemini-1.5-flash" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+    return { output, sandboxUrl };
+  }
+);
