6f8479480c60ef7f7f11fbccc5231a8e5efb2446
Harshalvk <harshalvkhobragade@gmail.com>
2025-06-28 13:04:20 +0530

    feat: setup trpc

--- src/components/Providers/AppProvider.tsx
+++ src/components/Providers/AppProvider.tsx
@@ -1,17 +1,20 @@
 import React, { ReactNode } from "react";
 import ThemeProvider from "./ThemeProvider";
+import { TRPCReactProvider } from "@/trpc/client";
 
 const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <ThemeProvider
-        attribute={"class"}
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
+      <TRPCReactProvider>
+        <ThemeProvider
+          attribute={"class"}
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </TRPCReactProvider>
     </>
   );
 };
