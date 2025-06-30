09addcbc7e2e933b334162d7d844919f2e8520b2
Harshalvk <harshalvkhobragade@gmail.com>
2025-06-30 23:05:38 +0530

    feat: implement message router with create and getMany functionalities

--- src/trpc/procedures/messages/server/procedures.ts
+++ src/trpc/procedures/messages/server/procedures.ts
@@ -16,12 +16,17 @@ export const messageRouter = createTRPCRouter({
   create: baseProcedure
     .input(
       z.object({
-        value: z.string().min(1, { message: "Message is required" }),
+        value: z
+          .string()
+          .min(1, { message: "Prompt is required" })
+          .max(10000, "Prompt is too long"),
+        projectId: z.string().min(1, { message: "Project ID is required" }),
       })
     )
     .mutation(async ({ input }) => {
       const createdMessage = await prisma.message.create({
         data: {
+          projectId: input.projectId,
           content: input.value,
           role: "USER",
           type: "RESULT",
@@ -32,6 +37,7 @@ export const messageRouter = createTRPCRouter({
         name: "code-agent/run",
         data: {
           value: input.value,
+          projectId: input.projectId
         },
       });
 
