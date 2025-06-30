f85581a0904344fe196b5fe0afb674c08c348d9a
Harshalvk <harshalvkhobragade@gmail.com>
2025-06-30 23:06:02 +0530

    feat: add projects router for project management functionalities

---
+++ src/trpc/procedures/projects/server/procedures.ts
@@ -0,0 +1,52 @@
+import { inngest } from "@/inngest/client";
+import { prisma } from "@/lib/prisma";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { z } from "zod";
+import { generateSlug } from "random-word-slugs";
+
+export const projectsRouter = createTRPCRouter({
+  getMany: baseProcedure.query(async () => {
+    const messages = await prisma.project.findMany({
+      orderBy: {
+        updatedAt: "desc",
+      },
+    });
+
+    return messages;
+  }),
+  create: baseProcedure
+    .input(
+      z.object({
+        value: z
+          .string()
+          .min(1, { message: "Prompt is required" })
+          .max(10000, "Prompt is too long"),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const createProject = await prisma.project.create({
+        data: {
+          name: generateSlug(2, {
+            format: "kebab",
+          }),
+          message: {
+            create: {
+              content: input.value,
+              role: "USER",
+              type: "RESULT",
+            },
+          },
+        },
+      });
+
+      await inngest.send({
+        name: "code-agent/run",
+        data: {
+          value: input.value,
+          projectId: createProject.id,
+        },
+      });
+
+      return createProject;
+    }),
+});
