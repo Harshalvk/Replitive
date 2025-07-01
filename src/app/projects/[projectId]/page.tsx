0b883e58cb8b8387c3f483c4a07a79e8ac7bbec7
Harshalvk <harshalvkhobragade@gmail.com>
2025-07-01 23:20:36 +0530

    feat: create ProjectPage component to handle project-specific message loading

---
+++ src/app/projects/[projectId]/page.tsx
@@ -0,0 +1,34 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import React, { Suspense } from "react";
+import ProjectView from "../_components/ProjectView";
+
+interface Props {
+  params: Promise<{ projectId: string }>;
+}
+
+const ProjectPage = async ({ params }: Props) => {
+  const { projectId } = await params;
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.messages.getMany.queryOptions({
+      projectId,
+    })
+  );
+  void queryClient.prefetchQuery(
+    trpc.projects.getOne.queryOptions({
+      id: projectId,
+    })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ProjectView projectId={projectId} />
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+
+export default ProjectPage;
